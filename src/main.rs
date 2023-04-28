f606ba1c52272e6585089755d6ea7beff78e866c
Dániel Buga <bugadani@gmail.com>
2023-04-28 10:17:02 +0200

    Set up embassy, log, and a temo task

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
 extern crate alloc;
 
+use embassy_executor::{Executor, _export::StaticCell};
+use embassy_time::{Duration, Ticker};
 use esp_backtrace as _;
-use esp_println::println;
 
 #[cfg(feature = "esp32s2")]
-use esp32s2_hal as hal;
+pub use esp32s2_hal as hal;
 
 #[cfg(feature = "esp32s3")]
-use esp32s3_hal as hal;
+pub use esp32s3_hal as hal;
 
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+use esp_println::logger::init_logger;
+use futures_util::StreamExt;
+use hal::{
+    clock::{ClockControl, CpuClock},
+    embassy,
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+    Rtc,
+};
 
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
@@ -36,23 +47,36 @@ fn init_heap() {
     }
 }
 
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
 #[entry]
 fn main() -> ! {
     init_heap();
+    init_logger(log::LevelFilter::Info);
+
     let peripherals = Peripherals::take();
+
     let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock240MHz).freeze();
 
-    // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
     rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
-    println!("Hello world!");
 
-    loop {}
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timer_group0.timer0);
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(ticker_task()).ok();
+    });
+}
+
+#[embassy_executor::task]
+async fn ticker_task() {
+    let mut ticker = Ticker::every(Duration::from_millis(500));
+    loop {
+        ticker.next().await;
+        log::info!("Tick");
+        ticker.next().await;
+        log::info!("Tock");
+    }
 }
