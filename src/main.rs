e593173a69eb65a314d609aadb6ca635a35714ff
Dániel Buga <bugadani@gmail.com>
2023-05-02 19:39:24 +0200

    Pass display to task

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,65 @@ use esp_println::logger::init_logger;
 use display_interface_spi_async::SPIInterface;
 use hal::{
     clock::{ClockControl, CpuClock},
-    dma::DmaPriority,
+    dma::{ChannelRx, ChannelTx, DmaPriority},
     embassy,
-    gdma::Gdma,
+    gdma::{Gdma, *},
+    gpio::{
+        Bank0GpioRegisterAccess, GpioPin, InputOutputAnalogPinType, Output, PushPull,
+        SingleCoreInteruptStatusRegisterAccessBank0,
+    },
     peripherals::Peripherals,
     prelude::*,
-    spi::SpiMode,
+    soc::gpio::{Gpio10Signals, Gpio13Signals},
+    spi::{dma::SpiDma, FullDuplexMode, SpiMode},
     timer::TimerGroup,
     Rtc, Spi, IO,
 };
-use ssd1306::{rotation::DisplayRotation, size::DisplaySize128x64, Ssd1306};
+use ssd1306::{
+    mode::BufferedGraphicsMode, rotation::DisplayRotation, size::DisplaySize128x64, Ssd1306,
+};
 
 mod heap;
 
 use crate::heap::init_heap;
 
 static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
+type DisplaySpi<'d> = SpiDma<
+    'd,
+    hal::peripherals::SPI2,
+    ChannelTx<'d, Channel0TxImpl, esp32s3_hal::gdma::Channel0>,
+    ChannelRx<'d, Channel0RxImpl, esp32s3_hal::gdma::Channel0>,
+    SuitablePeripheral0,
+    FullDuplexMode,
+>;
+
+type DisplayDataCommand = GpioPin<
+    Output<PushPull>,
+    Bank0GpioRegisterAccess,
+    SingleCoreInteruptStatusRegisterAccessBank0,
+    InputOutputAnalogPinType,
+    Gpio13Signals,
+    13,
+>;
+type DisplayChipSelect = GpioPin<
+    Output<PushPull>,
+    Bank0GpioRegisterAccess,
+    SingleCoreInteruptStatusRegisterAccessBank0,
+    InputOutputAnalogPinType,
+    Gpio10Signals,
+    10,
+>;
+
+type Display<'a> = Ssd1306<
+    SPIInterface<DisplaySpi<'a>, DisplayDataCommand, DisplayChipSelect>,
+    DisplaySize128x64,
+    BufferedGraphicsMode<DisplaySize128x64>,
+>;
+
+struct Resources {
+    display: Display<'static>,
+}
 
 #[entry]
 fn main() -> ! {
@@ -72,8 +115,8 @@ fn main() -> ! {
 
     display_cs.connect_peripheral_to_output(display_spi.cs_signal());
 
-    let mut descriptors = [0u32; 8 * 3];
-    let mut rx_descriptors = [0u32; 8 * 3];
+    static mut DISPLAY_SPI_DESCRIPTORS: [u32; 24] = [0u32; 8 * 3];
+    static mut DISPLAY_SPI_RX_DESCRIPTORS: [u32; 24] = [0u32; 8 * 3];
     let display_spi = Spi::new_no_cs_no_miso(
         display_spi,
         display_sclk,
@@ -85,28 +128,30 @@ fn main() -> ! {
     )
     .with_dma(display_dma_channel.configure(
         false,
-        &mut descriptors,
-        &mut rx_descriptors,
+        unsafe { &mut DISPLAY_SPI_DESCRIPTORS },
+        unsafe { &mut DISPLAY_SPI_RX_DESCRIPTORS },
         DmaPriority::Priority0,
     ));
 
     let display_interface = SPIInterface::new(display_spi, display_dc, display_cs);
 
-    let _display = Ssd1306::new(
+    let display = Ssd1306::new(
         display_interface,
         DisplaySize128x64,
         DisplayRotation::Rotate0,
     )
     .into_buffered_graphics_mode();
 
+    let resources = Resources { display };
+
     let executor = EXECUTOR.init(Executor::new());
-    executor.run(|spawner| {
-        spawner.spawn(ticker_task()).ok();
+    executor.run(move |spawner| {
+        spawner.spawn(ticker_task(resources)).ok();
     });
 }
 
 #[embassy_executor::task]
-async fn ticker_task() {
+async fn ticker_task(mut resources: Resources) {
     let mut ticker = Ticker::every(Duration::from_millis(500));
     loop {
         ticker.next().await;
