c1257af24a2ce79d932bd16c1c8053eccd6ce819
Dániel Buga <bugadani@gmail.com>
2023-05-24 19:52:39 +0200

    Stop tasks when state exits

--- src/states/wifi_ap.rs
+++ src/states/wifi_ap.rs
@@ -1,8 +1,10 @@
 use embassy_executor::Spawner;
+use embassy_futures::select::select;
 use embassy_net::{
     tcp::TcpSocket, Config, IpListenEndpoint, Ipv4Address, Ipv4Cidr, Stack, StackResources,
     StaticConfig,
 };
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
 use embassy_time::{Duration, Ticker, Timer};
 use embedded_graphics::Drawable;
 use embedded_io::asynch::Write;
@@ -51,12 +53,24 @@ pub async fn wifi_ap(board: &mut Board) -> AppState {
 
     let spawner = Spawner::for_current_executor().await;
 
-    spawner.spawn(connection_task(controller)).ok();
+    let connection_task_signal = Signal::<NoopRawMutex, ()>::new();
+    let net_task_signal = Signal::<NoopRawMutex, ()>::new();
+    let webserver_task_signal = Signal::<NoopRawMutex, ()>::new();
+
     spawner
-        .spawn(net_task(unsafe { as_static_ref(&stack) }))
+        .spawn(connection_task(controller, unsafe {
+            as_static_ref(&connection_task_signal)
+        }))
         .ok();
     spawner
-        .spawn(webserver_task(unsafe { as_static_ref(&stack) }))
+        .spawn(net_task(unsafe { as_static_ref(&stack) }, unsafe {
+            as_static_ref(&net_task_signal)
+        }))
+        .ok();
+    spawner
+        .spawn(webserver_task(unsafe { as_static_ref(&stack) }, unsafe {
+            as_static_ref(&webserver_task_signal)
+        }))
         .ok();
 
     let mut screen = WifiApScreen::new(
@@ -92,114 +106,145 @@ pub async fn wifi_ap(board: &mut Board) -> AppState {
         ticker.next().await;
     }
 
+    connection_task_signal.signal(());
+    net_task_signal.signal(());
+    webserver_task_signal.signal(());
+
     AppState::MainMenu
 }
 
 #[embassy_executor::task]
-async fn connection_task(mut controller: WifiController<'static>) {
-    log::debug!("start connection task");
-    log::debug!("Device capabilities: {:?}", controller.get_capabilities());
-    loop {
-        match esp_wifi::wifi::get_wifi_state() {
-            WifiState::ApStart => {
-                // wait until we're no longer connected
-                controller.wait_for_event(WifiEvent::ApStop).await;
-                Timer::after(Duration::from_millis(5000)).await
-            }
-            _ => {}
-        }
-        if !matches!(controller.is_started(), Ok(true)) {
-            let client_config = Configuration::AccessPoint(AccessPointConfiguration {
-                ssid: "Card/IO".into(),
-                ..Default::default()
-            });
-            controller.set_configuration(&client_config).unwrap();
-            log::debug!("Starting wifi");
-            controller.start().await.unwrap();
-            log::debug!("Wifi started!");
-        }
-    }
-}
+async fn connection_task(
+    mut controller: WifiController<'static>,
+    token: &'static Signal<NoopRawMutex, ()>,
+) {
+    select(
+        async {
+            log::debug!("start connection task");
+            log::debug!("Device capabilities: {:?}", controller.get_capabilities());
 
-#[embassy_executor::task]
-async fn net_task(stack: &'static Stack<WifiDevice<'static>>) {
-    stack.run().await
-}
+            loop {
+                match esp_wifi::wifi::get_wifi_state() {
+                    WifiState::ApStart => {
+                        // wait until we're no longer connected
+                        controller.wait_for_event(WifiEvent::ApStop).await;
+                        Timer::after(Duration::from_millis(5000)).await;
 
-#[embassy_executor::task]
-async fn webserver_task(stack: &'static Stack<WifiDevice<'static>>) {
-    let mut rx_buffer = [0; 4096];
-    let mut tx_buffer = [0; 4096];
-
-    loop {
-        if stack.is_link_up() {
-            break;
-        }
-        Timer::after(Duration::from_millis(500)).await;
-    }
-
-    let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
-    socket.set_timeout(Some(embassy_net::SmolDuration::from_secs(10)));
-    loop {
-        log::info!("Wait for connection...");
-        let r = socket
-            .accept(IpListenEndpoint {
-                addr: None,
-                port: 8080,
-            })
-            .await;
-        log::info!("Connected...");
-
-        if let Err(e) = r {
-            log::warn!("connect error: {:?}", e);
-            continue;
-        }
-
-        let mut buffer = [0u8; 1024];
-        let mut pos = 0;
-        loop {
-            match socket.read(&mut buffer).await {
-                Ok(0) => {
-                    log::info!("read EOF");
-                    break;
-                }
-                Ok(len) => {
-                    let to_print =
-                        unsafe { core::str::from_utf8_unchecked(&buffer[..(pos + len)]) };
-
-                    if to_print.contains("\r\n\r\n") {
-                        log::debug!("Received: {}", to_print);
-                        break;
+                        // TODO: exit app state if disconnected?
                     }
-
-                    pos += len;
+                    _ => {}
                 }
-                Err(e) => {
-                    log::warn!("read error: {:?}", e);
-                    break;
+
+                if !matches!(controller.is_started(), Ok(true)) {
+                    let client_config = Configuration::AccessPoint(AccessPointConfiguration {
+                        ssid: "Card/IO".into(),
+                        ..Default::default()
+                    });
+                    controller.set_configuration(&client_config).unwrap();
+                    log::debug!("Starting wifi");
+
+                    controller.start().await.unwrap();
+                    log::debug!("Wifi started!");
                 }
-            };
-        }
+            }
+        },
+        token.wait(),
+    )
+    .await;
+}
 
-        let r = socket
-            .write_all(
-                b"HTTP/1.0 200 OK\r\n\r\n\
-            <html>\
-                <body>\
-                    <h1>Hello Rust! Hello esp-wifi!</h1>\
-                </body>\
-            </html>\r\n\
-            ",
-            )
-            .await;
-        if let Err(e) = r {
-            log::warn!("write error: {:?}", e);
-        }
+#[embassy_executor::task]
+async fn net_task(
+    stack: &'static Stack<WifiDevice<'static>>,
+    token: &'static Signal<NoopRawMutex, ()>,
+) {
+    select(stack.run(), token.wait()).await;
+}
 
-        if let Err(e) = socket.flush().await {
-            log::warn!("flush error: {:?}", e);
-        }
+#[embassy_executor::task]
+async fn webserver_task(
+    stack: &'static Stack<WifiDevice<'static>>,
+    token: &'static Signal<NoopRawMutex, ()>,
+) {
+    select(
+        async {
+            let mut rx_buffer = [0; 4096];
+            let mut tx_buffer = [0; 4096];
 
-        socket.close();
-    }
+            while !stack.is_link_up() {
+                Timer::after(Duration::from_millis(500)).await;
+            }
+
+            let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
+            socket.set_timeout(Some(embassy_net::SmolDuration::from_secs(10)));
+
+            loop {
+                log::info!("Wait for connection...");
+
+                let r = socket
+                    .accept(IpListenEndpoint {
+                        addr: None,
+                        port: 8080,
+                    })
+                    .await;
+
+                log::info!("Connected...");
+
+                if let Err(e) = r {
+                    log::warn!("connect error: {:?}", e);
+                    continue;
+                }
+
+                let mut buffer = [0u8; 1024];
+                let mut pos = 0;
+                loop {
+                    match socket.read(&mut buffer).await {
+                        Ok(0) => {
+                            log::info!("read EOF");
+                            break;
+                        }
+                        Ok(len) => {
+                            let to_print =
+                                unsafe { core::str::from_utf8_unchecked(&buffer[..(pos + len)]) };
+
+                            if to_print.contains("\r\n\r\n") {
+                                log::debug!("Received: {}", to_print);
+                                break;
+                            }
+
+                            pos += len;
+                        }
+                        Err(e) => {
+                            log::warn!("read error: {:?}", e);
+                            break;
+                        }
+                    };
+                }
+
+                let r = socket
+                    .write_all(
+                        b"HTTP/1.0 200 OK\r\n\r\n\
+                        <html>\
+                            <body>\
+                                <h1>Hello Rust! Hello esp-wifi!</h1>\
+                            </body>\
+                        </html>\r\n\
+                        ",
+                    )
+                    .await;
+
+                if let Err(e) = r {
+                    log::warn!("write error: {:?}", e);
+                }
+
+                if let Err(e) = socket.flush().await {
+                    log::warn!("flush error: {:?}", e);
+                }
+
+                socket.close();
+            }
+        },
+        token.wait(),
+    )
+    .await;
 }
