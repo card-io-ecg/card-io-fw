fb90f0a80b2286095fabc56335e61e5fd8c24ba8
Dániel Buga <bugadani@gmail.com>
2023-05-26 20:59:28 +0200

    Load headers, reorganize, call handler

--- bad-server/src/lib.rs
+++ bad-server/src/lib.rs
@@ -4,6 +4,7 @@
 
 use embassy_net::{tcp::TcpSocket, IpListenEndpoint};
 use embedded_io::asynch::Write;
+use httparse::Status;
 use object_chain::{Chain, ChainElement, Link};
 
 use crate::{
@@ -14,37 +15,39 @@ use crate::{
 pub mod handler;
 pub mod method;
 
-pub struct BadServer<H: Handler, const REQUEST_BUFFER: usize> {
+pub struct BadServer<H: Handler, const REQUEST_BUFFER: usize, const MAX_HEADERS: usize> {
     handler: H,
 }
 
-impl<'s> BadServer<(), 1024> {
+impl<'s> BadServer<(), 1024, 32> {
     pub fn new() -> Self {
         Self { handler: () }
     }
 
-    pub fn add_handler<H: Handler>(self, handler: H) -> BadServer<Chain<H>, 1024> {
+    pub fn add_handler<H: Handler>(self, handler: H) -> BadServer<Chain<H>, 1024, 32> {
         BadServer {
             handler: Chain::new(handler),
         }
     }
 }
 
-impl<H, const REQUEST_BUFFER: usize> BadServer<Chain<H>, REQUEST_BUFFER>
+impl<H, const REQUEST_BUFFER: usize, const MAX_HEADERS: usize>
+    BadServer<Chain<H>, REQUEST_BUFFER, MAX_HEADERS>
 where
     H: Handler,
 {
     pub fn add_handler<H2: Handler>(
         self,
         handler: H2,
-    ) -> BadServer<Link<H2, Chain<H>>, REQUEST_BUFFER> {
+    ) -> BadServer<Link<H2, Chain<H>>, REQUEST_BUFFER, MAX_HEADERS> {
         BadServer {
             handler: self.handler.append(handler),
         }
     }
 }
 
-impl<H, P, const REQUEST_BUFFER: usize> BadServer<Link<H, P>, REQUEST_BUFFER>
+impl<H, P, const REQUEST_BUFFER: usize, const MAX_HEADERS: usize>
+    BadServer<Link<H, P>, REQUEST_BUFFER, MAX_HEADERS>
 where
     H: Handler,
     P: ChainElement + Handler,
@@ -52,18 +55,29 @@ where
     pub fn add_handler<H2: Handler>(
         self,
         handler: H2,
-    ) -> BadServer<Link<H2, Link<H, P>>, REQUEST_BUFFER> {
+    ) -> BadServer<Link<H2, Link<H, P>>, REQUEST_BUFFER, MAX_HEADERS> {
         BadServer {
             handler: self.handler.append(handler),
         }
     }
 }
 
-impl<H, const REQUEST_BUFFER: usize> BadServer<H, REQUEST_BUFFER>
+impl<H, const REQUEST_BUFFER: usize, const MAX_HEADERS: usize>
+    BadServer<H, REQUEST_BUFFER, MAX_HEADERS>
 where
     H: Handler,
 {
-    pub fn with_buffer_size<const NEW_BUFFER_SIZE: usize>(self) -> BadServer<H, NEW_BUFFER_SIZE> {
+    pub fn with_buffer_size<const NEW_BUFFER_SIZE: usize>(
+        self,
+    ) -> BadServer<H, NEW_BUFFER_SIZE, MAX_HEADERS> {
+        BadServer {
+            handler: self.handler,
+        }
+    }
+
+    pub fn with_header_count<const NEW_HEADER_COUNT: usize>(
+        self,
+    ) -> BadServer<H, REQUEST_BUFFER, NEW_HEADER_COUNT> {
         BadServer {
             handler: self.handler,
         }
@@ -86,104 +100,116 @@ where
         }
     }
 
-    async fn handle(&self, socket: &mut TcpSocket<'_>) {
-        let mut buffer = [0u8; REQUEST_BUFFER];
+    async fn load_headers(
+        &self,
+        buffer: &mut [u8],
+        socket: &mut TcpSocket<'_>,
+    ) -> Result<(usize, usize), ()> {
         let mut pos = 0;
-
-        loop {
-            let len = match socket.read(&mut buffer).await {
+        while pos < buffer.len() {
+            match socket.read(&mut buffer[pos..]).await {
                 Ok(0) => {
                     // We're here because the previous read wasn't a complete request. Reading 0
                     // means the request will not ever be completed.
                     log::warn!("read EOF");
-                    break;
+                    return Err(());
                 }
-                Ok(len) => len,
+                Ok(len) => pos += len,
                 Err(e) => {
                     log::warn!("read error: {:?}", e);
-                    break;
+                    return Err(());
                 }
-            };
+            }
 
-            pos += len;
-            log::info!("Buffer size: {pos}");
+            log::debug!("Buffer size: {pos}");
 
-            let mut headers = [httparse::EMPTY_HEADER; 20];
+            let mut headers = [httparse::EMPTY_HEADER; MAX_HEADERS];
             let mut req = httparse::Request::new(&mut headers);
 
-            let res = match req.parse(&buffer) {
-                Ok(res) => res,
+            match req.parse(&buffer[0..pos]) {
+                Ok(Status::Complete(header_size)) => return Ok((header_size, pos)),
+                Ok(Status::Partial) => {
+                    // We need to read more
+                }
                 Err(_) => {
                     log::warn!("Parsing request failed");
-                    socket.close();
-                    continue;
+                    return Err(());
                 }
             };
+        }
 
-            if let (Some(method), Some(path)) = (req.method, req.path) {
-                let Some(method) = Method::new(method) else {
-                    log::warn!("Unknown method {method}");
+        // Can't read more, but we don't have a complete request yet.
+        Err(())
+    }
+
+    async fn handle(&self, socket: &mut TcpSocket<'_>) {
+        let mut buffer = [0u8; REQUEST_BUFFER];
+
+        match self.load_headers(&mut buffer, socket).await {
+            Ok((header_size, total_read)) => {
+                let mut headers = [httparse::EMPTY_HEADER; MAX_HEADERS];
+                let mut req = httparse::Request::new(&mut headers);
+
+                let (header_buf, body_buf) = buffer.split_at_mut(header_size);
+                req.parse(header_buf).unwrap();
+
+                let Some(path) = req.path else {
+                    log::warn!("Path not set");
                     // TODO: send a proper response
                     socket.close();
-                    continue;
+                    return;
                 };
 
-                // we can send 404 early if none of our handlers match
+                let Some(method) = req.method.and_then(Method::new) else {
+                    log::warn!("Unknown method: {:?}", req.method);
+                    // TODO: send a proper response
+                    socket.close();
+                    return;
+                };
+
                 let request = Request {
                     method,
                     path,
                     body: b"",
                 };
                 if !self.handler.handles(&request) {
-                    // TODO: response builder
-                    let r = socket
-                        .write_all(
-                            b"HTTP/1.0 404 Not Found\r\n\r\n\
-                                <html>\
-                                    <body>\
-                                        <h1>404 Not Found</h1>\
-                                    </body>\
-                                </html>\r\n\
-                                ",
-                        )
-                        .await;
-
-                    if let Err(e) = r {
-                        log::warn!("write error: {:?}", e);
-                    }
-
-                    if let Err(e) = socket.flush().await {
-                        log::warn!("flush error: {:?}", e);
-                    }
-
-                    pos = 0;
-                    continue;
+                    self.send_404(socket).await;
+                    return;
                 }
-            }
 
-            if res.is_complete() {
-                let r = socket
-                    .write_all(
-                        b"HTTP/1.0 200 OK\r\n\r\n\
+                // TODO: create a body reader that uses the loaded bytes,
+                // and reads more from socket when needed.
+                let read_body = total_read - header_size;
+                let _buf = &body_buf[0..read_body];
+                self.handler.handle(request).await;
+            }
+            Err(_) => todo!(),
+        }
+    }
+
+    async fn send_404(&self, socket: &mut TcpSocket<'_>)
+    where
+        H: Handler,
+    {
+        // TODO: response builder
+        let r = socket
+            .write_all(
+                b"HTTP/1.0 404 Not Found\r\n\r\n\
                         <html>\
                             <body>\
-                                <h1>Hello Rust! Hello esp-wifi!</h1>\
+                                <h1>404 Not Found</h1>\
                             </body>\
                         </html>\r\n\
                         ",
-                    )
-                    .await;
+            )
+            .await;
 
-                if let Err(e) = r {
-                    log::warn!("write error: {:?}", e);
-                }
+        if let Err(e) = r {
+            log::warn!("write error: {:?}", e);
+        }
 
-                if let Err(e) = socket.flush().await {
-                    log::warn!("flush error: {:?}", e);
-                }
-
-                pos = 0;
-            }
+        if let Err(e) = socket.flush().await {
+            log::warn!("flush error: {:?}", e);
         }
     }
 }
