7a287a7849a4c12d301517c56e83e1ad219f463b
Dániel Buga <bugadani@gmail.com>
2023-07-01 14:00:51 +0200

    Pick a file name to save to

--- src/states/upload_or_store_measurement.rs
+++ src/states/upload_or_store_measurement.rs
@@ -1,3 +1,6 @@
+use core::fmt::Write;
+
+use norfs::StorageError;
 use signal_processing::{buffer::Buffer, i24::i24};
 
 use crate::{board::initialized::Board, states::BIG_OBJECTS, AppState};
@@ -13,7 +16,9 @@ pub async fn upload_or_store_measurement(board: &mut Board, next_state: AppState
     let ecg = unsafe { BIG_OBJECTS.as_ecg() };
 
     if try_to_upload(board, &mut ecg.buffer).await == StoreMeasurement::Store {
-        try_store_measurement(board, &mut ecg.buffer).await;
+        if let Err(e) = try_store_measurement(board, &mut ecg.buffer).await {
+            log::error!("Failed to store measurement: {e:?}");
+        }
     }
 
     next_state
@@ -50,7 +55,58 @@ async fn try_to_upload<const N: usize>(
     StoreMeasurement::DontStore
 }
 
-async fn try_store_measurement<const N: usize>(_board: &mut Board, _buffer: &mut Buffer<i24, N>) {
+async fn try_store_measurement<const N: usize>(
+    board: &mut Board,
+    _buffer: &mut Buffer<i24, N>,
+) -> Result<(), StorageError> {
     log::debug!("Trying to store measurement");
+
+    let Some(storage) = &mut board.storage else {
+        return Ok(());
+    };
+
+    let mut max_index = None;
+    let mut dir = storage.read_dir().await?;
+    let mut buffer = [0; 64];
+    while let Some(file) = dir.next(storage).await? {
+        match file.name(storage, &mut buffer).await {
+            Ok(name) => {
+                if let Some(idx) = name
+                    .strip_prefix("meas.")
+                    .and_then(|s| s.parse::<u32>().ok())
+                {
+                    let update_max = if let Some(max) = max_index {
+                        idx > max
+                    } else {
+                        true
+                    };
+
+                    if update_max {
+                        max_index = Some(idx);
+                    }
+                }
+            }
+            Err(StorageError::InsufficientBuffer) => {
+                // not a measurement file, ignore
+            }
+            Err(e) => {
+                log::warn!("Failed to read file name: {:?}", e);
+                return Err(e);
+            }
+        }
+    }
+
+    let mut filename = heapless::String::<16>::new();
+    if write!(
+        &mut filename,
+        "meas.{}",
+        max_index.map(|idx| idx + 1).unwrap_or(0)
+    )
+    .is_err()
+    {
+        log::warn!("Failed to create filename");
+        return Ok(());
+    }
+
     unimplemented!()
 }
