c5fdf8c7dbe7acc52ec7d2ab406c7410acecc4b4
Dániel Buga <bugadani@gmail.com>
2023-07-06 20:46:24 +0200

    Update to use CompressingBuffer

--- signal-processing/src/compressing_buffer.rs
+++ signal-processing/src/compressing_buffer.rs
@@ -95,6 +95,10 @@ impl<const N: usize> CompressingBuffer<N> {
         }
     }
 
+    pub fn capacity(&self) -> usize {
+        N
+    }
+
     pub fn len(&self) -> usize {
         self.element_count
     }
@@ -116,6 +120,30 @@ impl<const N: usize> CompressingBuffer<N> {
         self.first_element = 0;
         self.last_element = 0;
         self.buffer.clear();
+    }
+
+    pub fn as_bytes(&self) -> (&[u8], &[u8]) {
+        let (a, b) = if self.write_idx > self.bytes {
+            let start = self.write_idx - self.bytes;
+            let end = self.write_idx;
+            (&self.buffer[start..end], &[][..])
+        } else {
+            let start = N - (self.bytes - self.write_idx);
+            (&self.buffer[start..], &self.buffer[..self.write_idx])
+        };
+
+        unsafe {
+            (
+                core::slice::from_raw_parts(
+                    a.as_ptr() as *const u8,
+                    a.len() * core::mem::size_of::<i32>(),
+                ),
+                core::slice::from_raw_parts(
+                    b.as_ptr() as *const u8,
+                    b.len() * core::mem::size_of::<i32>(),
+                ),
+            )
+        }
     }
 }
 
--- src/states/upload_or_store_measurement.rs
+++ src/states/upload_or_store_measurement.rs
@@ -1,7 +1,7 @@
 use core::fmt::Write;
 
 use norfs::{writer::FileDataWriter, OnCollision, StorageError};
-use signal_processing::{buffer::Buffer, i24::i24};
+use signal_processing::compressing_buffer::CompressingBuffer;
 
 use crate::{board::initialized::Board, states::BIG_OBJECTS, AppState};
 
@@ -26,9 +26,9 @@ pub async fn upload_or_store_measurement(board: &mut Board, next_state: AppState
 
 async fn try_to_upload<const N: usize>(
     board: &mut Board,
-    buffer: &mut Buffer<i24, N>,
+    buffer: &mut CompressingBuffer<N>,
 ) -> StoreMeasurement {
-    if buffer.len() < buffer.capacity() / 2 {
+    if buffer.len() < 15_000 {
         log::debug!("Buffer is too short to upload.");
         // We don't want to store too-short measurements.
         return StoreMeasurement::DontStore;
@@ -57,7 +57,7 @@ async fn try_to_upload<const N: usize>(
 
 async fn try_store_measurement<const N: usize>(
     board: &mut Board,
-    measurement: &mut Buffer<i24, N>,
+    measurement: &mut CompressingBuffer<N>,
 ) -> Result<(), StorageError> {
     log::debug!("Trying to store measurement");
 
@@ -123,7 +123,7 @@ async fn try_store_measurement<const N: usize>(
     Ok(())
 }
 
-struct MeasurementWriter<'a, const N: usize>(&'a Buffer<i24, N>);
+struct MeasurementWriter<'a, const N: usize>(&'a CompressingBuffer<N>);
 
 impl<const N: usize> FileDataWriter for MeasurementWriter<'_, N> {
     async fn write<M>(
