71620fb7427a30aa01d3998a6448ba9f1ef478e0
Dániel Buga <bugadani@gmail.com>
2023-07-26 21:06:57 +0200

    Track the number of elements instead of a buffer full flag

--- signal-processing/src/buffer.rs
+++ signal-processing/src/buffer.rs
@@ -2,7 +2,7 @@ use core::mem::MaybeUninit;
 
 pub struct Buffer<T: Copy, const N: usize> {
     write_idx: usize,
-    full: bool,
+    count: usize,
     buffer: [MaybeUninit<T>; N],
 }
 
@@ -18,22 +18,18 @@ impl<T: Copy, const N: usize> Buffer<T, N> {
     pub const fn new() -> Self {
         Self {
             write_idx: 0,
-            full: false,
+            count: 0,
             buffer: [MaybeUninit::uninit(); N],
         }
     }
 
     pub fn clear(&mut self) {
         self.write_idx = 0;
-        self.full = false;
+        self.count = 0;
     }
 
     pub fn len(&self) -> usize {
-        if self.full {
-            N
-        } else {
-            self.write_idx
-        }
+        self.count
     }
 
     pub fn is_empty(&self) -> bool {
@@ -45,25 +41,25 @@ impl<T: Copy, const N: usize> Buffer<T, N> {
     }
 
     pub fn is_full(&self) -> bool {
-        self.full
+        self.count == N
     }
 
     pub fn push(&mut self, sample: T) -> Option<T> {
         let old = self
-            .full
+            .is_full()
             .then_some(unsafe { self.buffer[self.write_idx].assume_init() });
 
         self.buffer[self.write_idx] = MaybeUninit::new(sample);
         self.write_idx = (self.write_idx + 1) % self.buffer.len();
-        if self.write_idx == 0 {
-            self.full = true;
+        if !self.is_full() {
+            self.count += 1;
         }
 
         old
     }
 
     pub fn iter(&self) -> impl Iterator<Item = T> + Clone + '_ {
-        let (start, end) = if !self.full {
+        let (start, end) = if !self.is_full() {
             (&self.buffer[0..self.write_idx], &[][..])
         } else {
             (
