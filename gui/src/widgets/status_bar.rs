540126f5e7e15e3b13bc78145e335f9e0b9168a7
Dániel Buga <bugadani@gmail.com>
2023-08-08 14:21:00 +0200

    Inline status bar fns

--- gui/src/widgets/status_bar.rs
+++ gui/src/widgets/status_bar.rs
@@ -20,10 +20,12 @@ pub struct StatusBar {
 }
 
 impl StatusBar {
+    #[inline]
     pub fn update_battery_style(&mut self, style: BatteryStyle) {
         self.battery.style = style;
     }
 
+    #[inline]
     pub fn update_battery_data(&mut self, battery_data: Option<BatteryInfo>) {
         self.battery.data = battery_data;
     }
@@ -33,6 +35,7 @@ impl Drawable for StatusBar {
     type Color = BinaryColor;
     type Output = ();
 
+    #[inline]
     fn draw<DT: DrawTarget<Color = BinaryColor>>(&self, display: &mut DT) -> Result<(), DT::Error> {
         LinearLayout::horizontal(*self).arrange().draw(display)
     }
