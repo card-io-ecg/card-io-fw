8674c3be89333fa60af5094f32974348fa893af0
Dániel Buga <bugadani@gmail.com>
2023-09-10 23:23:40 +0200

    Increase heap size

--- src/heap.rs
+++ src/heap.rs
@@ -1,13 +1,11 @@
-use core::mem::MaybeUninit;
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
 
 #[global_allocator]
 pub static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
 pub fn init_heap() {
-    const HEAP_SIZE: usize = 48 * 1024;
+    const HEAP_SIZE: usize = (48 + 96) * 1024;
     static mut HEAP: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-
-    use core::ptr::addr_of_mut;
 
     unsafe {
         let heap_size = HEAP.len();
