79a597e06c3fec36de19bf69cb2878e6d282e20e
Dániel Buga <bugadani@gmail.com>
2023-09-11 06:19:38 +0200

    Clean up

--- src/states/upload_or_store_measurement.rs
+++ src/states/upload_or_store_measurement.rs
@@ -7,7 +7,10 @@ use embassy_net::{
 };
 use embassy_time::{Duration, Timer};
 use embedded_nal_async::{Dns, TcpConnect};
-use norfs::{medium::StorageMedium, writer::FileDataWriter, OnCollision, Storage, StorageError};
+use norfs::{
+    medium::StorageMedium, read_dir::DirEntry, writer::FileDataWriter, OnCollision, Storage,
+    StorageError,
+};
 use reqwless::{client::HttpClient, request::RequestBuilder};
 use signal_processing::compressing_buffer::CompressingBuffer;
 use ufmt::uwrite;
@@ -15,7 +18,7 @@ use ufmt::uwrite;
 use crate::{
     board::{
         initialized::{Board, StaMode},
-        wifi::sta::ConnectionState,
+        wifi::sta::{ConnectionState, Sta},
     },
     states::display_message,
     AppState, SerialNumber,
@@ -33,8 +36,9 @@ pub async fn upload_or_store_measurement<const SIZE: usize>(
     mut buffer: Box<CompressingBuffer<SIZE>>,
     next_state: AppState,
 ) -> AppState {
-    if try_to_upload(board, &mut buffer).await == StoreMeasurement::Store {
-        let store_result = try_store_measurement(board, &mut buffer).await;
+    let samples = buffer.make_contiguous();
+    if try_to_upload(board, samples).await == StoreMeasurement::Store {
+        let store_result = try_store_measurement(board, samples).await;
         buffer.clear();
 
         if let Err(e) = store_result {
@@ -54,15 +58,11 @@ pub async fn upload_or_store_measurement<const SIZE: usize>(
 }
 
 struct Resources {
-    //request_buffer: [u8; 2048],
     client_state: TcpClientState<1, 1024, 1024>,
     rx_buffer: [u8; 1024],
 }
 
-async fn try_to_upload<const SIZE: usize>(
-    board: &mut Board,
-    buffer: &mut CompressingBuffer<SIZE>,
-) -> StoreMeasurement {
+async fn try_to_upload(board: &mut Board, buffer: &[u8]) -> StoreMeasurement {
     const SAMPLE_RATE: usize = 1000; // samples/sec
     if buffer.len() < 20 * SAMPLE_RATE {
         debug!("Buffer is too short to upload or store.");
@@ -75,27 +75,12 @@ async fn try_to_upload<const SIZE: usize>(
         return StoreMeasurement::Store;
     }
 
-    board.signal_sta_work_available();
-    let sta = if !board.config.known_networks.is_empty() {
-        board.enable_wifi_sta(StaMode::OnDemand).await
-    } else {
-        board.disable_wifi().await;
-        None
-    };
-
-    let Some(sta) = sta else {
+    let Some(sta) = enable_sta(board).await else {
         return StoreMeasurement::Store;
     };
 
-    if sta.connection_state() != ConnectionState::Connected {
-        while sta.wait_for_state_change().await == ConnectionState::Connecting {
-            display_message(board, "Connecting...").await;
-        }
-
-        if sta.connection_state() != ConnectionState::Connected {
-            // If we do not have a network connection, save to file.
-            return StoreMeasurement::Store;
-        }
+    if !wait_for_connection(&sta, board).await {
+        return StoreMeasurement::Store;
     }
 
     // If we found a network, attempt to upload.
@@ -106,11 +91,10 @@ async fn try_to_upload<const SIZE: usize>(
     unwrap!(uwrite!(
         &mut uploading_msg,
         "Uploading measurement: {} bytes",
-        buffer.byte_count()
+        buffer.len()
     ));
 
     display_message(board, uploading_msg.as_str()).await;
-    let samples = buffer.make_contiguous();
 
     let mut resources = Box::new(Resources {
         client_state: TcpClientState::new(),
@@ -125,7 +109,7 @@ async fn try_to_upload<const SIZE: usize>(
         &board.config.backend_url,
         &mut client,
         0,
-        samples,
+        buffer,
         &mut resources.rx_buffer,
     )
     .await
@@ -139,28 +123,12 @@ async fn try_to_upload<const SIZE: usize>(
 }
 
 async fn upload_stored(board: &mut Board) {
-    board.signal_sta_work_available();
-    let sta = if !board.config.known_networks.is_empty() {
-        // This call should handle the case where there are no files stored.
-        board.enable_wifi_sta(StaMode::OnDemand).await
-    } else {
-        board.disable_wifi().await;
-        None
-    };
-
-    let Some(sta) = sta else {
+    let Some(sta) = enable_sta(board).await else {
         return;
     };
 
-    if sta.connection_state() != ConnectionState::Connected {
-        while sta.wait_for_state_change().await == ConnectionState::Connecting {
-            display_message(board, "Connecting...").await;
-        }
-
-        if sta.connection_state() != ConnectionState::Connected {
-            // If we do not have a network connection, save to file.
-            return;
-        }
+    if !wait_for_connection(&sta, board).await {
+        return;
     }
 
     display_message(board, "Uploading stored measurements...").await;
@@ -193,39 +161,26 @@ async fn upload_stored(board: &mut Board) {
                 };
 
                 match file.name(storage, &mut fn_buffer).await {
-                    Ok(name) => {
-                        if name.starts_with("meas.") {
-                            let Ok(size) = file.size(storage).await else {
-                                warn!("Failed to read size of {}", name);
-                                continue;
-                            };
+                    Ok(name) if name.starts_with("meas.") => {
+                        let Ok(buffer) = load_measurement(file, storage).await else {
+                            warn!("Failed to load {}", name);
+                            continue;
+                        };
 
-                            let Ok(mut buffer) = buffer_with_capacity(size, 0) else {
-                                warn!("Failed to allocate {} bytes for {}", size, name);
-                                continue;
-                            };
-
-                            let mut reader = file.open().await;
-                            if let Err(e) = reader.read_all(storage, buffer.as_mut()).await {
-                                warn!("Failed to read {}: {:?}", name, e);
-                                continue;
-                            };
-
-                            if let Err(e) = upload_measurement(
-                                &board.config.backend_url,
-                                &mut client,
-                                0,
-                                buffer.as_ref(),
-                                &mut resources.rx_buffer,
-                            )
-                            .await
-                            {
-                                warn!("Failed to upload {}: {:?}", name, e);
-                                return;
-                            }
+                        if let Err(e) = upload_measurement(
+                            &board.config.backend_url,
+                            &mut client,
+                            0,
+                            buffer.as_ref(),
+                            &mut resources.rx_buffer,
+                        )
+                        .await
+                        {
+                            warn!("Failed to upload {}: {:?}", name, e);
+                            return;
                         }
                     }
-                    Err(StorageError::InsufficientBuffer) => {
+                    Ok(_) | Err(StorageError::InsufficientBuffer) => {
                         // not a measurement file, ignore
                     }
                     Err(e) => {
@@ -240,6 +195,56 @@ async fn upload_stored(board: &mut Board) {
             }
         }
     }
+}
+
+async fn enable_sta(board: &mut Board) -> Option<Sta> {
+    board.signal_sta_work_available();
+    if !board.config.known_networks.is_empty() {
+        // This call should handle the case where there are no files stored.
+        board.enable_wifi_sta(StaMode::OnDemand).await
+    } else {
+        board.disable_wifi().await;
+        None
+    }
+}
+
+async fn wait_for_connection(sta: &Sta, board: &mut Board) -> bool {
+    if sta.connection_state() != ConnectionState::Connected {
+        while sta.wait_for_state_change().await == ConnectionState::Connecting {
+            display_message(board, "Connecting...").await;
+        }
+
+        if sta.connection_state() != ConnectionState::Connected {
+            // If we do not have a network connection, save to file.
+            return false;
+        }
+    }
+
+    true
+}
+
+async fn load_measurement<M>(file: DirEntry<M>, storage: &mut Storage<M>) -> Result<Box<[u8]>, ()>
+where
+    M: StorageMedium,
+    [(); M::BLOCK_COUNT]:,
+{
+    let Ok(size) = file.size(storage).await else {
+        warn!("Failed to read size");
+        return Err(());
+    };
+
+    let Ok(mut buffer) = buffer_with_capacity(size, 0) else {
+        warn!("Failed to allocate {} bytes", size);
+        return Err(());
+    };
+
+    let mut reader = file.open().await;
+    if let Err(e) = reader.read_all(storage, buffer.as_mut()).await {
+        warn!("Failed to read data: {:?}", e);
+        return Err(());
+    };
+
+    Ok(buffer)
 }
 
 fn buffer_with_capacity<T: Copy>(size: usize, init_val: T) -> Result<Box<[T]>, ()> {
@@ -299,17 +304,14 @@ where
     Ok(())
 }
 
-async fn try_store_measurement<const SIZE: usize>(
-    board: &mut Board,
-    measurement: &mut CompressingBuffer<SIZE>,
-) -> Result<(), StorageError> {
+async fn try_store_measurement(board: &mut Board, measurement: &[u8]) -> Result<(), StorageError> {
     debug!("Trying to store measurement");
 
     let mut saving_msg = heapless::String::<48>::new();
     unwrap!(uwrite!(
         &mut saving_msg,
         "Saving measurement: {} bytes",
-        measurement.byte_count()
+        measurement.len()
     ));
     display_message(board, &saving_msg).await;
     let Some(storage) = board.storage.as_mut() else {
@@ -330,8 +332,6 @@ async fn try_store_measurement<const SIZE: usize>(
             OnCollision::Fail,
         )
         .await?;
-
-    measurement.clear();
 
     info!("Measurement saved to {}", filename);
 
@@ -379,41 +379,37 @@ where
     Ok(max_index.map(|idx| idx + 1).unwrap_or(0))
 }
 
-struct MeasurementWriter<'a, const N: usize>(&'a CompressingBuffer<N>);
+struct MeasurementWriter<'a>(&'a [u8]);
 
-impl<'a, const N: usize> MeasurementWriter<'a, N> {
+impl<'a> MeasurementWriter<'a> {
     // We're good with a straight u8 until 127 samples, then we can consider switching to varint.
     const FORMAT_VERSION: u8 = 0;
 }
 
-impl<const N: usize> FileDataWriter for MeasurementWriter<'_, N> {
+impl FileDataWriter for MeasurementWriter<'_> {
     async fn write<M>(
         &self,
         writer: &mut norfs::writer::Writer<M>,
         storage: &mut Storage<M>,
     ) -> Result<(), StorageError>
     where
-        M: norfs::medium::StorageMedium,
+        M: StorageMedium,
         [(); M::BLOCK_COUNT]:,
     {
         // Here we only store differences, but not the initial sample. The DC offset does not
         // matter for the analysis, and we can reconstruct everything else from the differences.
-        let buffers = self.0.as_slices();
 
         let mut writer = writer.bind(storage);
 
         writer
             .write_all(&Self::FORMAT_VERSION.to_le_bytes())
             .await?;
-        writer.write_all(buffers.0).await?;
-        writer.write_all(buffers.1).await?;
+        writer.write_all(self.0).await?;
 
         Ok(())
     }
 
     fn estimate_length(&self) -> usize {
-        let buffers = self.0.as_slices();
-
-        Self::FORMAT_VERSION.to_le_bytes().len() + buffers.0.len() + buffers.1.len()
+        Self::FORMAT_VERSION.to_le_bytes().len() + self.0.len()
     }
 }
