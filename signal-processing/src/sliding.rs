c5ad5c67e5bd7d2d0d0450b48ee6a2f6ff46ae9a
Dániel Buga <bugadani@gmail.com>
2023-10-07 22:06:41 +0200

    Add dynamic sliding window

--- signal-processing/src/sliding.rs
+++ signal-processing/src/sliding.rs
@@ -48,3 +48,60 @@ impl<const N: usize> SlidingWindow<N> {
         self.buffer.iter()
     }
 }
+
+#[cfg(feature = "alloc")]
+#[derive(Clone)]
+pub struct AllocSlidingWindow {
+    buffer: alloc::boxed::Box<[f32]>,
+    write_idx: usize,
+    count: usize,
+}
+
+#[cfg(feature = "alloc")]
+impl AllocSlidingWindow {
+    #[inline(always)]
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            buffer: alloc::vec![0.0; capacity].into_boxed_slice(),
+            write_idx: 0,
+            count: 0,
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.count = 0;
+        self.write_idx = 0;
+    }
+
+    pub fn len(&self) -> usize {
+        self.count
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.len() == self.buffer.len()
+    }
+
+    pub fn push(&mut self, sample: f32) -> Option<f32> {
+        let old = if self.count < self.buffer.len() {
+            self.count += 1;
+            None
+        } else {
+            Some(self.buffer[self.write_idx])
+        };
+        self.buffer[self.write_idx] = sample;
+        self.write_idx = (self.write_idx + 1) % self.buffer.len();
+        old
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = f32> + Clone + '_ {
+        let start = self.write_idx;
+        let end = start + self.count;
+        let len = self.buffer.len();
+        let buffer = &self.buffer;
+        (start..end).map(move |i| buffer[i % len])
+    }
+}
