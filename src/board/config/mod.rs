38e8c2d09966d3b3aa8cb75ea5d27644420546ac
Dániel Buga <bugadani@gmail.com>
2026-01-01 10:13:30 +0100

    Log config version and migration steps

--- src/board/config/mod.rs
+++ src/board/config/mod.rs
@@ -40,18 +40,23 @@ impl ConfigFile {
     #[inline(never)]
     pub fn into_config(mut self) -> Config {
         if let Self::V1(config) = self {
+            info!("Migrating config data to v2");
             self = Self::V2(v2::Config::from(config));
         }
         if let Self::V2(config) = self {
+            info!("Migrating config data to v3");
             self = Self::V3(v3::Config::from(config));
         }
         if let Self::V3(config) = self {
+            info!("Migrating config data to v4");
             self = Self::V4(v4::Config::from(config));
         }
         if let Self::V4(config) = self {
+            info!("Migrating config data to v5");
             self = Self::V5(v5::Config::from(config));
         }
         if let Self::V5(config) = self {
+            info!("Migrating config data to latest");
             self = Self::Current(Config::from(config));
         }
 
@@ -64,7 +69,9 @@ impl ConfigFile {
 
 impl Loadable for ConfigFile {
     async fn load<R: Read>(reader: &mut R) -> Result<Self, LoadError<R::Error>> {
-        let data = match u8::load(reader).await? {
+        let version = u8::load(reader).await?;
+        info!("Loading config data with version {}", version + 1);
+        let data = match version {
             0 => Self::V1(v1::Config::load(reader).await?),
             1 => Self::V2(v2::Config::load(reader).await?),
             2 => Self::V3(v3::Config::load(reader).await?),
