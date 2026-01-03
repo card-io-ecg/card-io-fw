aeb4be1d83d023fc2a108249526baa9a9560ad47
Dániel Buga <bugadani@gmail.com>
2026-01-03 15:19:44 +0100

    Skip compressing config site if unused

--- config-site/src/handlers/mod.rs
+++ config-site/src/handlers/mod.rs
@@ -4,23 +4,39 @@ pub mod change_backend_url;
 pub mod delete_network;
 pub mod list_known_networks;
 
-use bad_server::{handler::StaticHandler, Header};
+#[cfg(feature = "compress")]
+mod statics {
+    use bad_server::{handler::StaticHandler, Header};
 
-pub const INDEX_HANDLER: StaticHandler = StaticHandler::new(
-    &[Header {
-        name: "Content-Encoding",
-        value: b"gzip",
-    }],
-    include_bytes!(concat!(env!("COMPRESS_OUT_DIR"), "/static/index.html.gz")),
-);
+    pub const INDEX_HANDLER: StaticHandler = StaticHandler::new(
+        &[Header {
+            name: "Content-Encoding",
+            value: b"gzip",
+        }],
+        include_bytes!(concat!(env!("COMPRESS_OUT_DIR"), "/static/index.html.gz")),
+    );
 
-pub const HEADER_FONT: StaticHandler = StaticHandler::new(
-    &[Header {
-        name: "Content-Encoding",
-        value: b"gzip",
-    }],
-    include_bytes!(concat!(
-        env!("COMPRESS_OUT_DIR"),
-        "/static/Poppins-Regular.ttf.gz"
-    )),
-);
+    pub const HEADER_FONT: StaticHandler = StaticHandler::new(
+        &[Header {
+            name: "Content-Encoding",
+            value: b"gzip",
+        }],
+        include_bytes!(concat!(
+            env!("COMPRESS_OUT_DIR"),
+            "/static/Poppins-Regular.ttf.gz"
+        )),
+    );
+}
+
+#[cfg(not(feature = "compress"))]
+mod statics {
+    use bad_server::handler::StaticHandler;
+
+    pub const INDEX_HANDLER: StaticHandler =
+        StaticHandler::new(&[], include_bytes!("../../static/index.html"));
+
+    pub const HEADER_FONT: StaticHandler =
+        StaticHandler::new(&[], include_bytes!("../../static/Poppins-Regular.ttf"));
+}
+
+pub use statics::*;
